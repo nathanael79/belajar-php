456f1d820c450d7a6cc8481edae95001d6600d30
eko.k.khannedy <eko.k.khannedy@gdn-commerce.com>
2019-04-10 18:26:35 +0700

    learning if control structure

---
+++ if_control_structure.php
@@ -0,0 +1,21 @@
+<?php
+
+$name = "Eko";
+
+if ($name == "Eko"):
+    echo "Hai Eko" . PHP_EOL;
+    echo "Mau Kemana?" . PHP_EOL;
+elseif ($name == "Budi") :
+    echo "Hai Eko" . PHP_EOL;
+    echo "Mau Kemana?" . PHP_EOL;
+elseif ($name == "Joko"):
+
+else :
+
+endif;
+
+?>
+
+<?php if ($name == "Eko") : ?>
+    Hello Eko
+<?php endif; ?>
