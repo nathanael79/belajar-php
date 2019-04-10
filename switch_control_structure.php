29e6b1909902c0854dcc61ac662216a7ceda5d80
eko.k.khannedy <eko.k.khannedy@gdn-commerce.com>
2019-04-10 19:05:33 +0700

    learning switch control structure

---
+++ switch_control_structure.php
@@ -0,0 +1,18 @@
+<?php
+
+$name = "Budi";
+
+switch ($name) {
+    case "Eko":
+        echo("Hai Eko");
+        break;
+    case "Kurniawan" :
+        echo("Hai Kurniawan");
+        break;
+    case "Khannedy":
+        echo("Hai Khannedy");
+        break;
+    default:
+        echo("Siapa ya?");
+        break;
+}
